c3b742e3b51a8e83ecaee66db6bbd701490bb8e3
arya.mohades <arya.mohades@kibocommerce.com>
2018-10-22 16:18:36 -0500

    add some comments

--- app/containers/Form/saga.js
+++ app/containers/Form/saga.js
@@ -14,31 +14,45 @@ import {
  */
 export function* submit(formKey, endpoint, data, onSuccess, onError) {
   try {
+    // Wait for the submit form api call to complete
     const response = yield call(request, endpoint, {
       method: 'POST',
       body: JSON.stringify(data),
     });
 
+    // If request is successful, dispatch the submit success action
+    // with the response data
     yield put(submitFormSuccess(formKey, response));
 
+    // If onSuccess action type is provided, dispatch it
+    // with the response data
     if (onSuccess) {
       yield put({ type: onSuccess, response });
     }
   } catch (error) {
+    // If request errors, dispatch the submit error action
+    // with the error
     yield put(submitFormError(formKey, error));
 
+    // If onError action type is provided, dispatch it
+    // with the error
     if (onError) {
       yield put({ type: onError, error });
     }
   }
 }
 
+/**
+ * Submit form flow
+ */
 export default function* submitFlow() {
   while (true) {
+    // Wait for a submit form request
     const { formKey, endpoint, data, onSuccess, onError } = yield take(
       SUBMIT_FORM,
     );
 
+    // Fork a task to make the submit request api call
     const task = yield fork(
       submit,
       formKey,
@@ -48,12 +62,14 @@ export default function* submitFlow() {
       onError,
     );
 
+    // Wait for one of cancel submit, submit success, or submit error actions
     const action = yield take([
       CANCEL_SUBMIT_FORM,
       SUBMIT_FORM_SUCCESS,
       SUBMIT_FORM_ERROR,
     ]);
 
+    // If action type is cancel submit, then cancel the submit task
     if (action.type === CANCEL_SUBMIT_FORM) {
       yield cancel(task);
     }
